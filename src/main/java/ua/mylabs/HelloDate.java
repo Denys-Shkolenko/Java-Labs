20996045374d06b5361f3955b5485cf01bc8a9e8
Denys-Shkolenko <denys.shkolenko@gmail.com>
2022-09-08 19:20:08 +0300

    add: HelloDate class

---
+++ src/main/java/ua/mylabs/HelloDate.java
@@ -0,0 +1,11 @@
+package ua.mylabs;
+
+import java.util.Date;
+
+public class HelloDate {
+    
+    public static void main(String[] args) {
+        System.out.println("Hey, today is ");
+        System.out.println(new Date());
+    }
+}
